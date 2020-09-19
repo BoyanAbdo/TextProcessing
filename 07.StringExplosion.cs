68f3fe860db83eb940d31b26f47b0be7c79223f9
Boyan Abdo <boyan.abdo@gmail.com>
2020-09-19 10:23:39 +0100

    Add files via upload

---
+++ 07.StringExplosion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+
+namespace _07.StringExplosion
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine(); // 1234
+            //StringBuilder sb = new StringBuilder();
+            //string result = string.Empty;
+
+            int count = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (input[i] == '>')
+                {
+
+                    if (i != input.Length - 1 && Int32.TryParse(input[i + 1] + "", out int str))
+                    {
+                        int strength = int.Parse(input[i + 1] + "");
+                        strength += count;
+                        count = 0;
+                        for (int j = 0; j < strength; j++)
+                        {
+                            if ((i + 1) >= input.Length || input[i + 1] == '>')
+                            {
+                                break;
+                            }
+                            else
+                            {
+
+                                input = input.Remove(i + 1, 1);
+                                count++;
+                            }
+                        }
+                        count = strength - count; // if str = 2, count = 2 => count = 0
+                    }
+
+                }
+
+
+            }
+            Console.WriteLine(input);
+
+        }
+    }
+}
---
+++ 08.LettersChangeNumbers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _08.LettersChangeNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            string inputLine = Console.ReadLine();
+            string[] tokens = inputLine.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            // P34562Z q2576f   H456z
+
+            decimal sum = 0;
+            foreach (var token in tokens)
+            {
+                int len = token.Length;
+                char prefix = token[0];
+                char suffix = token[len - 1];
+                int number = int.Parse(token.Substring(1, len - 2));
+
+                decimal currentSum = 0;
+
+                if (prefix >=65 && prefix <= 90) // uppercase
+                {
+                    decimal divider = prefix - 64;
+                    currentSum += number / divider;
+                }
+                else if (prefix>=97 && prefix<=122) //lowercase
+                {
+                    decimal multiplier = prefix - 96;
+                    currentSum += number * multiplier;
+                }
+
+                if (suffix >= 65 && suffix<= 90) // uppercase
+                {
+                    currentSum -= (suffix - 64);
+                }
+                else if (suffix >= 97 && suffix <= 122) //lowercase
+                {
+                    currentSum += (suffix - 96);
+                }
+
+                sum += currentSum;
+            }
+            Console.WriteLine($"{sum:F2}");
+
+
+
+
+        }
+    }
+}
