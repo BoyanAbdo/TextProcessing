49490ad5ab8c4b322303efa46fc0b70786cc472c
Boyan Abdo <boyan.abdo@gmail.com>
2020-09-24 22:00:36 +0100

    Update 02.CharacterMultiplier.cs

--- 02.CharacterMultiplier.cs
+++ 02.CharacterMultiplier.cs
@@ -3,6 +3,15 @@ using System.Linq;
 
 namespace _02.CharacterMultiplier
 {
+
+// Create a method that takes two strings as arguments and returns the sum of their character codes 
+// multiplied (multiply str1[0] with str2[0] and add to the total sum). 
+// Then continue with the next two characters. If one of the strings is longer than the other, 
+// add the remaining character codes to the total sum without multiplication.
+// Gosho Pesho      ->      53253
+// 123 522          ->      7647
+
+
     class Program
     {
         static void Main()
