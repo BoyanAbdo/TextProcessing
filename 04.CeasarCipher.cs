8adfb20dd037d558d46844c0c11c7cc4387d45fe
Boyan Abdo <boyan.abdo@gmail.com>
2020-09-26 07:48:15 +0100

    Update 04.CeasarCipher.cs

--- 04.CeasarCipher.cs
+++ 04.CeasarCipher.cs
@@ -2,6 +2,13 @@
 
 namespace _04.CaesarCipher
 {
+
+// Write a program that returns an encrypted version of the same text. 
+// Encrypt the text by shifting each character with three positions forward. 
+// For example A would be replaced by D, B would become E, and so on. Print the encrypted text.
+// Programming is cool!     ->      Surjudpplqj#lv#frro$
+
+
     class Program
     {
         static void Main(string[] args)
