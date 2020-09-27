4619ad5422068d1b0313d1d6e02b62e03cc033e3
Boyan Abdo <boyan.abdo@gmail.com>
2020-09-27 10:49:03 +0100

    Update 07.StringExplosion.cs

--- 07.StringExplosion.cs
+++ 07.StringExplosion.cs
@@ -3,13 +3,18 @@ using System.Text;
 
 namespace _07.StringExplosion
 {
+
+// Explosions are marked with '>'. Immediately after the mark, there will be an integer, which signifies the strength of the explosion.
+// You should remove x characters (where x is the strength of the explosion), starting after the punch character ('>').
+// If you find another explosion mark ('>') while you’re deleting characters, you should add the strength to your previous explosion.
+// When all characters are processed, print the string without the deleted characters. 
+// You should not delete the explosion character – '>', but you should delete the integers, which represent the strength. 
+   
     class Program
     {
         static void Main()
         {
-            string input = Console.ReadLine(); // 1234
-            //StringBuilder sb = new StringBuilder();
-            //string result = string.Empty;
+            string input = Console.ReadLine();
 
             int count = 0;
             for (int i = 0; i < input.Length; i++)
